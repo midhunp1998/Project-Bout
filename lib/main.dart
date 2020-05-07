3ae9b00615ed7f08e961355879a5b76fbacb30c4
midhunprakash <midhunprakash@deakin.edu.au>
2020-05-07 11:58:25 +1000

    Bout Project initial Files

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+void main() => runApp( Firstpage());
+
+class Firstpage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+      home:  Scaffold(
+        body: Container(
+
+          decoration: BoxDecoration(
+            gradient: LinearGradient(colors: [const Color(0xFFffd194),const Color(0xFF70e1f5)],
+                begin: FractionalOffset.topLeft,
+                end: FractionalOffset.bottomRight,
+                stops: [0.1,1.0],
+                tileMode: TileMode.clamp
+            ),
+          ),
+
+            child: Center(
+              child: Image(
+                image: AssetImage('assets/bout1.png'),
+        ),
+      ),
+    ),
+    ),
+    );
+  }
+}
+
+
+
